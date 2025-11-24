6d21da851a59d5a0995f3dc210719a70cc129487
nicot <nicot@Nicols>
2025-11-24 09:53:06 +0100

    Estructura: Clases ValidadorIBAN e IBANApp creadas

---
+++ src/validadoriban/ValidadorIBAN.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package validadoriban;
+
+/**
+ *
+ * @author nicot
+ */
+
+public class ValidadorIBAN {
+    // Clase reservada para la logica matematica
+}
